62a687d877a9805a291c1d10aea68314d5087e27
mpichlmann <14205@coderacademy.edu.au>
2023-08-23 15:49:41 +1000

    added more students

--- seed.js
+++ seed.js
@@ -4,6 +4,7 @@ const saltRounds = 10
 
 const seedData = async () => {
     try {
+        // Delete all previous data
         await AssessmentModel.deleteMany()
         console.log('Deleted assessments')
 
@@ -15,7 +16,8 @@ const seedData = async () => {
 
         await SkillModel.deleteMany()
         console.log('Deleted skills')
-
+        
+        // Skills data
         const skillslist = [
             { skillName: 'jump', level: 1 },
             { skillName: 'handstand', level: 2 },
@@ -28,7 +30,8 @@ const seedData = async () => {
         ]
         const sk = await SkillModel.insertMany(skillslist)
         console.log('Skills seeded')
-
+        
+        // User data
         const users = [
             { 
             username: 'eliteadmin', 
@@ -43,10 +46,6 @@ const seedData = async () => {
             isAdmin: false 
             }
         ]
-        // const us = await UserModel.insertMany(users)
-        // console.log('Users seeded')
-
-        // Hash passwords before inserting
         const hashedUsers = await Promise.all(users.map(async (user) => {
         const hashedPassword = await bcrypt.hash(user.password, saltRounds)
         return {
@@ -54,22 +53,28 @@ const seedData = async () => {
             password: hashedPassword
             }
         }))
-
         const us = await UserModel.insertMany(hashedUsers);
         console.log('Users seeded')
         
-
-
-
-
+        // Student data
         const students = [
             { name: 'Lachie', DOB: new Date('1996-09-04'), skillLevel: 3 },
             { name: 'Argine', DOB: new Date('1994-01-01'), skillLevel: 6 },
-            { name: 'Max', DOB: new Date('1996-07-04'), skillLevel: 1 }
+            { name: 'Max', DOB: new Date('1996-07-04'), skillLevel: 1 },
+            { name: 'Mushu', DOB: new Date('1996-09-04'), skillLevel: 3 },
+            { name: 'Clyde', DOB: new Date('1994-01-01'), skillLevel: 6 },
+            { name: 'Mr Dingus', DOB: new Date('1996-07-04'), skillLevel: 1 },
+            { name: 'Phil', DOB: new Date('1996-09-04'), skillLevel: 3 },
+            { name: 'Guido', DOB: new Date('1994-01-01'), skillLevel: 6 },
+            { name: 'Barbie', DOB: new Date('1996-07-04'), skillLevel: 1 },
+            { name: 'Britta', DOB: new Date('1996-09-04'), skillLevel: 3 },
+            { name: 'Luffy', DOB: new Date('1994-01-01'), skillLevel: 6 },
+            { name: 'Shirley', DOB: new Date('1996-07-04'), skillLevel: 1 },
         ]
         const st = await StudentModel.insertMany(students);
         console.log('Students seeded');
 
+        // Assessment Data
         const assessments = [
             {
                 student: st.find(student => student.name === 'Lachie')._id,
@@ -125,7 +130,7 @@ const seedData = async () => {
                     { skill: (await SkillModel.findOne({ skillName: 'backflip' }))._id, score: 2 }
                 ]
             }
-        ];
+        ]
         const as = await AssessmentModel.insertMany(assessments)
         console.log('Assessments seeded')
     } catch (error) {
